a11ec0e6ffb61dd6a5ede9fa66f5dc7adb0c6024
Renata Tsai <ranata47204720@gmail.com>
2020-10-03 15:52:10 +0800

    done with this project

---
+++ chat.py
@@ -0,0 +1,35 @@
+
+def read_file(filename):
+	lines = []
+	with open(filename, 'r' , encoding='utf-8-sig') as f:
+		for line in f:
+			lines.append(line.strip())
+	return lines
+
+def convert(lines):
+	new = []
+	person = None
+	for line in lines:
+		if line == 'Allen':
+			person = 'Allen'
+			continue
+		elif line == 'Tom':
+			person = 'Tom'
+			continue
+#如果person有value才能執行以下的程式
+		if person:
+			new.append(person + ': ' + line)
+	return new
+
+
+def write_file(filename, lines):
+	with open(filename, 'w') as f:
+		for line in lines:
+			f.write(line + '\n')
+
+def main():
+	lines = read_file('input.txt')
+	lines = convert(lines)
+	write_file('output.txt', lines)
+
+main()
